486f6672bd0c6169a70a5e7be814f9b7d4f04a4f
Paul Grayson <pdg@alum.mit.edu>
2011-09-03 22:53:12 -0700

    first test

--- each_selection_of/spec.rb
+++ each_selection_of/spec.rb
@@ -1,1 +1,11 @@
 require_relative 'kata'
+
+describe "Array" do
+  context "given an array with two elements" do
+    a = [1]
+
+    it "responds to each_selection_of" do
+      a.should respond_to :each_selection_of
+    end
+  end
+end
